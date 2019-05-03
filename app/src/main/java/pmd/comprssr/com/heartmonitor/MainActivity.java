6b19b16031aadd91bfb3bd1cd771b1eb57f582ab
Pranav <pranavmadavadev78@gmail.com>
2019-05-03 23:13:21 +0530

    Initial commit

---
+++ app/src/main/java/pmd/comprssr/com/heartmonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package pmd.comprssr.com.heartmonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
