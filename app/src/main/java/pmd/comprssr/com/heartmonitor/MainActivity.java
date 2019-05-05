81609c7ebd20d8405362630b58f0f1f59ff5d9d1
Preveen Raj <97preveenraj@gmail.com>
2019-05-05 12:38:45 +0530

    fixed every view

--- app/src/main/java/pmd/comprssr/com/heartmonitor/MainActivity.java
+++ app/src/main/java/pmd/comprssr/com/heartmonitor/MainActivity.java
@@ -2,12 +2,18 @@ package pmd.comprssr.com.heartmonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.patients_list);
+        setContentView(R.layout.show_device_waveform);
+
+//        Toast.makeText(this, "heyyy", Toast.LENGTH_SHORT).show();
+        FancyToast.makeText(this,"Logged in Successfully",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
     }
 }
